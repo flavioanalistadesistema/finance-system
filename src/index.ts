bae5d59c2e5b603cef9259a9b88d119a03dadbdc
Flávio Santos <flavioanalistadesistema@gmail.com>
2024-06-19 18:27:05 -0300

    Config do projeto, funcao insert, testes unitarios, config migrations, config knex/banco, config API/ROUTER express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World! testando pela segunda vez");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import RegistrarTransacaoController from "./controllers/RegistrarTransacaoController";
+import RegistrarTransicao from "./app/transacoes/RegistrarTransicao";
+import TransicaoDB from "./adaptadores/knex/TransicaoDB";
+
+const app = express();
+app.use(express.json());
+
+const registrarTransicaoPorta = new TransicaoDB();
+const registrarTransacao = new RegistrarTransicao(registrarTransicaoPorta);
+const registrarTransacaoController = new RegistrarTransacaoController(
+  app,
+  registrarTransacao
+);
+
+const porta = process.env.PORTA || 3002;
+app.listen(porta, () => {
+  console.log(`Server is running on port ${porta}`);
+});
--- test/soma.test.ts
+++
@@ -1,6 +0,0 @@
-import { Somar } from "../src/calc";
-
-// Teste unitário
-test("Testando function SOMA", () => {
-  expect(Somar(1, 2)).toBe(3);
-});
